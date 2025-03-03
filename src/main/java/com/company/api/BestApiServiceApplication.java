795fd94bb51b60a4e842df88ac4330eb266d7663
Arjun .S <145590376+Arjuns20004@users.noreply.github.com>
2025-03-03 16:12:32 +0530

    FC

---
+++ src/main/java/com/company/api/BestApiServiceApplication.java
@@ -0,0 +1,12 @@
+package com.company.api;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestApiServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BestApiServiceApplication.class, args);
+    }
+}
